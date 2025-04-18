d3fd4c14aa5a2ee15f39c2355111260d0850104e
MuhammetOzcan34 <muhammet.ozcann83@gmail.com>
2025-04-18 16:54:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateClock() {
+  const now = new Date();
+  const clock = document.getElementById("clock");
+  clock.innerText = now.toLocaleTimeString();
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+async function fetchRSS() {
+  const rssToJson = "https://api.rss2json.com/v1/api.json?rss_url=";
+  const feedURL = "https://www.trthaber.com/manset_articles.rss";
+  try {
+    const res = await fetch(rssToJson + encodeURIComponent(feedURL));
+    const data = await res.json();
+    const headlines = data.items.map(item => item.title).join(" | ");
+    document.getElementById("newsTicker").innerText = headlines;
+  } catch (e) {
+    document.getElementById("newsTicker").innerText = "Haberler yüklenemedi.";
+  }
+}
+fetchRSS();
